6391b5b19b7123ec0b40493657feab73e9c8b9dc
hertelukas <hertelukas@users.noreply.github.com>
2020-03-07 16:08:47 +0100

    Basic design scheme

--- Komment/Komment/MainWindow.xaml.cs
+++ Komment/Komment/MainWindow.xaml.cs
@@ -17,7 +17,10 @@ namespace Komment
         {
             var userLoggedIn = await UserData.LoadUser();
             if (userLoggedIn)
+            {
                 NetworkHandler.Initialize();
+                UsernameTextBlock.Text = $"Hello {User.username}!";
+            }
             else
             {
                 LoginWindow loginWindow = new LoginWindow();
@@ -35,5 +38,52 @@ namespace Komment
                 _ = Logger.LogInfo($"Loaded {User.Data.notes.Count.ToString()} notes");
             }
         }
+
+        #region UI Elements
+
+        //Buttons
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        //Other UI
+        private void TopBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+        }
+
+        private void MoveCursorMenu(int index)
+        {
+            TrainsitionigContentSlide.OnApplyTemplate();
+            GridCursor.Margin = new Thickness(0, 60 + (60 * index), 0, 0);
+        }
+
+        #endregion
+
+
     }
 }
